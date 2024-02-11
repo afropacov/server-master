863c19a92b430e98b45f9bc8e062b6408cc4d0f8
Ivsha10 <duricicsolutions@gmail.com>
2024-02-10 20:00:10 -0500

    third commit

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,7 +3,8 @@ const allowedOrigins =
     'https://kaleidoscopic-quokka-436365.netlify.app',
     'http://localhost:3000',
     'http://127.0.0.1:5500/',
-    'https://david-capstone-project.netlify.app'
+    'https://david-capstone-project.netlify.app',
+    'https://qk782k57r6.execute-api.us-east-2.amazonaws.com/STAGE1'
 ]
 
 module.exports = allowedOrigins;
