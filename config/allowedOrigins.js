97a16f66a0795d44100ef2d14865a0252f5427d7
Ivsha10 <duricicsolutions@gmail.com>
2024-02-10 19:22:22 -0500

    new commit

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,6 +3,7 @@ const allowedOrigins =
     'https://kaleidoscopic-quokka-436365.netlify.app',
     'http://localhost:3000',
     'http://127.0.0.1:5500/',
+    'https://david-capstone-project.netlify.app'
 ]
 
 module.exports = allowedOrigins;
