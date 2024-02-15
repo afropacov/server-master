b1aa6ab41955cc3336953202b46af9d59a39c4db
David <148135978+afropacov@users.noreply.github.com>
2024-02-14 19:40:01 -0500

    Update corsOptions.js

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,15 +1,6 @@
-const allowedOrigins = require('./allowedOrigins');
-
 const corsOptions = {
-    origin:(origin, callback) => {
-        if(allowedOrigins.indexOf(origin) !== 1 || !origin) {
-            callback(null, true)
-        } else {
-            callback(new Error('Not Allowed by CORS!'));
-            console.log('terajse', origin);
-        }
-    }, credentials: true,
+    origin: '*', // Allow access from any origin
+    credentials: true,
     optionsSuccesStatus: 200
-}
-
+};
 module.exports = corsOptions;
